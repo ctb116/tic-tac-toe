ad9c9557e70fc4a44ad53f5353762f976567a7b2
Catherine Bradley and Hyewon Cho <catherine@>
2018-08-22 16:17:35 -0700

    Finish tic-tac-toe business logic

--- js/scripts.js
+++ js/scripts.js
@@ -87,12 +87,12 @@ Board.prototype.horizontalWinner = function() {
 }
 
 Board.prototype.diagonalWinner = function() {
-  if (this.space[0][0].marked === true && this.space[1][1].marked === true && this.space[2][2].marked === true) {
+  if (this.spaces[0][0].marked === true && this.spaces[1][1].marked === true && this.spaces[2][2].marked === true) {
     if(this.spaces[0][0].markedBy === this.spaces[1][1].markedBy && this.spaces[1][1].markedBy === this.spaces[2][2].markedBy) {
       return this.spaces[0][0].markedBy;
     }
   }
-  if (this.space[0][2].marked === true && this.space[1][1].marked === true && this.space[2][0].marked === true) {
+  if (this.spaces[0][2].marked === true && this.spaces[1][1].marked === true && this.spaces[2][0].marked === true) {
     if(this.spaces[0][2].markedBy === this.spaces[1][1].markedBy && this.spaces[1][1].markedBy === this.spaces[2][0].markedBy) {
       return this.spaces[0][2].markedBy;
     }
@@ -101,31 +101,77 @@ Board.prototype.diagonalWinner = function() {
   return "";
 }
 
+var board = new Board();
 var playerX = new Player("X");
 var playerO = new Player("O");
 
+var currentPlayer = playerX;
+
+var switchPlayer = function() {
+  if (currentPlayer === playerX) {
+    currentPlayer = playerO;
+  }
+  else {
+    currentPlayer = playerX;
+  }
+}
+
+var addClick = function(divId) {
+  $(divId).click(function() {
+    //$(divId) = <div id="space10" class="col-md-4">
+    var idValue = $(this).attr('id');
+    //if idValue = "space10"
+    var rowIndex = idValue.charAt(5);
+    //rowIndex = "1"
+    var colIndex = idValue.charAt(6);
+    //colIndex = "0"
+    var clickedSpace = board.spaces[rowIndex][colIndex];
+    //if clicked space is already marked by player
+    if (clickedSpace.marked === true) {
+      console.log("this space is clicked");
+    }
+    //if clicked space hasn't marked
+    else {
+      clickedSpace.marked = true;
+      clickedSpace.markedBy = currentPlayer.mark;
+      var winnerMark = board.findWinner();
+      if (winnerMark !== "") {
+        //alert winner?
+        console.log("winner is " + winnerMark);
+      }
+    }
+    $(divId).text(clickedSpace.markedBy);
+    switchPlayer();
+  });
+}
 
 $(document).ready(function(){
-  var board = new Board();
-
   for (var row = 0; row < 3; row++) {
     var currentRow = [];
     for (var col = 0; col < 3; col++){
-      divId = "#space" + row + col;
+      var divId = "#space" + row + col;
       var newSpace = new Space(row, col);
       // makes a new space and sets its index with current row index and current column index
       currentRow.push(newSpace);
       // sends the new space to currentRow array.
       // push row and col to var currentRow
-      $(divId).click(function() {
-        console.log("clicked");
-      });
+      //divId = "#space10"
+
+      //call addClick function to attach jquery click function to every space object
+      //if I attach click function directly, in other words, attach it without addClick function,
+      //an error occurs
+      //Actually computer implements the function inside click() when each space is clicked,
+      //and at that time, row is 2, and column is 2 because computer already completed iterating two for loops.
+      //that's the reason why our program always thinks we clicked space22.
+      //We can prevent that kind of error by attaching click() through another function.
+      //If you want to get detailed information about this, search "javascript closure".
+      addClick(divId);
       // after three iterations, currentRow array will be filled with three spaces
     }
     board.spaces.push(currentRow);
   }
 
+
+
   console.log(board.spaces);
-  console.log(board.spaces[0]);
-  console.log(board.spaces[0][0]);
 });
